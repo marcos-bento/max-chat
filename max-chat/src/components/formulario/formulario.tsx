a49ff1c9381fd1b152b9276effe9729c3abe62c3
Marcos Bento <marcos.x.marcos@hotmail.com>
2023-10-31 14:58:06 -0300

    Finalização do card: Validação do e-mail com RegEx

--- max-chat/src/components/formulario/formulario.tsx
+++ max-chat/src/components/formulario/formulario.tsx
@@ -10,47 +10,64 @@ function Formulario({ type = "" }: { type?: string }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [nome, setNome] = useState(""); // Se você quiser rastrear o nome, se aplicável
+  const [emailValido, setEmailValido] = useState(true);
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const novoEmail = e.target.value;
+    setEmail(novoEmail);
+    validarEmail(novoEmail);
   };
 
-  const handleSenhaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSenha(event.target.value);
+  const validarEmail = (email: string) => {
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    setEmailValido(regexEmail.test(email));
   };
 
   const handleNomeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNome(event.target.value);
   };
 
+  const handleSenhaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSenha(event.target.value);
+  };
+
   const handleFormSubmit = async () => {
     // Valida o login
-    if (await validaLogin({ email, senha })){
+    if (emailValido) {
+      if (await validaLogin({ email, senha })){
         alert('Usuário logado com sucesso!');
         window.location.href="/menu"
+      } else {
+          alert('Dados inválidos! Tente novamente.');
+      };
     } else {
-        alert('Dados inválidos! Tente novamente.');
-    };
+      alert("Email inválido:");
+    }
   };
 
   const handleFormSubmitRegister = async () => {
     // Valida o registro
-    if (email != "" && senha != "" && nome != ""){
-      const resultado = await RegistraUsuario({ email, senha, nome })
-      alert(resultado.texto);
-      if (resultado.resul){
-        window.location.href="/menu"
+    if (emailValido) {
+      if (email != "" && senha != "" && nome != ""){
+        const resultado = await RegistraUsuario({ email, senha, nome })
+        alert(resultado.texto);
+        if (resultado.resul){
+          window.location.href="/menu"
+        }
+      } else {
+        alert('Preencha todos os campos para registrar um usuário!')
       }
     } else {
-      alert('Preencha todos os campos para registrar um usuário!')
+      alert("Email inválido:");
     }
- 
   };
 
   return (
     <div className={type === "" ? style.formulario : style.formulario_medio}>
       <p className={style.formulario_texto}>Login:</p>
       <Input placeholder={"Digite seu e-mail"} onChange={handleEmailChange} />
+      {!emailValido && <p className={style.alerta_erro} style={{ color: "salmon" }}>Email inválido</p>}
       <p className={style.formulario_texto}>Senha:</p>
       <Input placeholder={"Digite sua senha"} type={"password"} onChange={handleSenhaChange}/>
       {type === "" ? null : (
@@ -59,7 +76,6 @@ function Formulario({ type = "" }: { type?: string }) {
           <Input placeholder={"Digite seu nome"} onChange={handleNomeChange}/>
         </>
       )}
-
       <Botao texto={type === "" ? "Entrar" : "Registrar"} onClick={type === "" ? handleFormSubmit : handleFormSubmitRegister}/>
     </div>
   );
