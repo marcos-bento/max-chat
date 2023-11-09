e5f0acb8c2595773397ff0a877b71cf265697b44
Marcos Bento <marcos.x.marcos@hotmail.com>
2023-11-09 19:20:32 -0300

    Ajuste server.js

--- max-chat/api/server.js
+++ max-chat/api/server.js
@@ -1,25 +1,31 @@
-// JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-// Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
+const server = jsonServer.create()
 
-server.use(middlewares);
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
 // Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
-
-server.use(router);
-// Listen to port
-server.listen(3001, () => {
- console.log("JSON Server is running");
-});
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
