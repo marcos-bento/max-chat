75a6bab12779add05a9d5d61d9dc2e4d4e286287
Marcos Bento <marcos.x.marcos@hotmail.com>
2023-11-09 19:14:12 -0300

    Ajuste para rodar no Vercel

--- max-chat/api/server.js
+++ max-chat/api/server.js
@@ -1,20 +1,24 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
 
-server.use(middlewares)
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
-
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(3001, () => {
+ console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
