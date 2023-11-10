97001c2edd6b5726b192bac102fbcfea227b10e6
Marcos Bento <marcos.x.marcos@hotmail.com>
2023-11-10 08:43:15 -0300

    Ajuste final para rodar no Vercel

--- max-chat/src/Services/conectaApi.tsx
+++ max-chat/src/Services/conectaApi.tsx
@@ -1,31 +1,30 @@
 import { Cadastro } from "../Interfaces/cadastro";
 import { Conversa } from "../Interfaces/conversa";
-import { ConversaChat } from "../Interfaces/conversaChat";
 
 // Retorna todos usuários
 async function recuperaUsuario (){
-    const conexao = await fetch("http://localhost:3000/user");
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/user");
     const conexaoConvertida = await conexao.json();
     return { conexaoConvertida, statusConexao: conexao.status };
 };
 
 // Retorna um usuario específico por ID
 async function recuperaUsuarioPorID (id: number){
-    const conexao = await fetch("http://localhost:3000/user/"+id);
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/user/"+id);
     const conexaoConvertida = await conexao.json();
     return { conexaoConvertida, statusConexao: conexao.status };
 };
 
 // Retorna um usuario específico por EMAIL
 async function recuperaUsuarioPorEmail(email: string) {
-    const conexao = await fetch("http://localhost:3000/user?email=" + email);
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/user?email=" + email);
     const conexaoConvertida = await conexao.json();
     return { conexaoConvertida, statusConexao: conexao.status };
 }
 
 // Atualiza um usuario especifico por ID
 async function atualizaUsuario (id: number, cadastro: Cadastro){
-    const conexao = await fetch("http://localhost:3000/user/"+id, {
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/user/"+id, {
         method: "PUT",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(cadastro)
@@ -37,7 +36,7 @@ async function atualizaUsuario (id: number, cadastro: Cadastro){
 
 // Insere um novo usuário
 async function cadastraUsuario (cadastro: Cadastro){
-    const conexao = await fetch("http://localhost:3000/user", {
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/user", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(cadastro)
@@ -49,7 +48,7 @@ async function cadastraUsuario (cadastro: Cadastro){
 
 // Insere uma nova conversa
 async function registraConversa (conversa: Conversa){
-    const conexao = await fetch("http://localhost:3000/chats", {
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/chats", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(conversa)
@@ -61,7 +60,7 @@ async function registraConversa (conversa: Conversa){
 
 // Atualiza uma conversa existente por ID
 async function atualizaConversa (id: number, conversa: Conversa){
-    const conexao = await fetch("http://localhost:3000/chats/"+id, {
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/chats/"+id, {
         method: "PUT",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(conversa)
@@ -73,14 +72,14 @@ async function atualizaConversa (id: number, conversa: Conversa){
 
 // Retorna todas conversas
 async function recuperaConversa (){
-    const conexao = await fetch("http://localhost:3000/chats/");
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/chats/");
     const conexaoConvertida = await conexao.json();
     return { conexaoConvertida, statusConexao: conexao.status };
 };
 
 // Retorna uma conversa específica por ID
 async function recuperaChat (id: number){
-    const conexao = await fetch("http://localhost:3000/chats/"+id);
+    const conexao = await fetch("https://max-chat-json-server.vercel.app/chats/"+id);
     const conexaoConvertida = await conexao.json();
     return { conexaoConvertida, statusConexao: conexao.status };
 };
--- max-chat/start-server.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-
-exec('json-server --watch db.json', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Erro ao iniciar o servidor JSON: ${error}`);
-    return;
-  }
-  console.log(`Servidor JSON iniciado: ${stdout}`);
-});
